b84b95bc068e7fd48e9cf98f88bdac49710f2fa7
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-05-18 09:52:17 +0300

    SoftUni Fundamentals task

---  lab_data_types_and_variables/centuries_to_minutes.py
+++  lab_data_types_and_variables/centuries_to_minutes.py
@@ -4,9 +4,9 @@ ONE_CENTURIES_YEARS = 100
 ONE_CENTURIES_DAYS = 365.2422
 
 centuries_years = centuries * ONE_CENTURIES_YEARS
-centuries_days = centuries_years * ONE_CENTURIES_DAYS
+centuries_days = int(centuries_years) * ONE_CENTURIES_DAYS
 centuries_hours = int(centuries_days) * 24
-centuries_minutes = centuries_hours * 60
+centuries_minutes = int(centuries_hours) * 60
 
-print(f"{centuries:.0f} centuries = {centuries_years:.0f} years ="
-      f" {centuries_days:.0f} days = {centuries_hours:.0f} hours = {centuries_minutes:.0f} minutes")
+print(f"{centuries} centuries = {int(centuries_years)} years ="
+      f" {centuries_days} days = {centuries_hours} hours = {centuries_minutes} minutes")
