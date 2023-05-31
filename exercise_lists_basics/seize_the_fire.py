2006c98a2d30563a733152e85aafdc2cd79e33dd
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-05-31 10:45:37 +0300

    SoftUni Fundamentals task

--- exercise_lists_basics/seize_the_fire.py
+++ exercise_lists_basics/seize_the_fire.py
@@ -4,29 +4,29 @@ list_fire = []
 total_water = 0
 total_fire = 0
 effort = 0
+print("Cells:")
 
 for level in range(len(fire)):
-    current_fire = fire[level].split("=")
-    list_fire += fire[level].split("=")
-    while water >= 0:
-        if current_fire[0] == "High" and 81 <= int(current_fire[1]) <= 125:
+    current_fire = fire[level].split(" = ")
+    list_fire += fire[level].split(" = ")
+    if current_fire[0] == "High" and 81 <= int(current_fire[1]) <= 125:
+        if water - int(current_fire[1]) >= 0:
             total_fire += int(current_fire[1])
             water -= int(current_fire[1])
             effort += 0.25 * int(current_fire[1])
-        elif current_fire[0] == "Medium" and 51 <= int(current_fire[1]) <= 80:
+            print(f" - {current_fire[1]}")
+    elif current_fire[0] == "Medium" and 51 <= int(current_fire[1]) <= 80:
+        if water - int(current_fire[1]) >= 0:
             total_fire += int(current_fire[1])
             water -= int(current_fire[1])
             effort += 0.25 * int(current_fire[1])
-        elif current_fire[0] == "Low" and 1 <= int(current_fire[1]) <= 50:
+            print(f" - {current_fire[1]}")
+    elif current_fire[0] == "Low" and 1 <= int(current_fire[1]) <= 50:
+        if water - int(current_fire[1]) >= 0:
             total_fire += int(current_fire[1])
             water -= int(current_fire[1])
             effort += 0.25 * int(current_fire[1])
-        else:
-            break
+            print(f" - {current_fire[1]}")
 
-print("Cells:")
-for index in range(len(list_fire)):
-    if index % 2 != 0:
-        print(f" - {list_fire[index]}")
-print(f"Effort: {effort}")
+print(f"Effort: {effort:.2f}")
 print(f"Total Fire: {total_fire}")
