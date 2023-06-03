1ab0e099358136cca9583bab9586ec56fc783e72
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-06-03 19:39:19 +0300

    SoftUni Fundamentals task

--- exercise_functions/odd_and_even_sum.py
+++ exercise_functions/odd_and_even_sum.py
@@ -1,15 +1,16 @@
 
-def make_sum_of_all_number():
-    single_number = input()
+def make_sum_of_all_number(current_number):
     sum_of_even_digits = 0
     sum_of_odd_digits = 0
-    for digit in range(len(single_number)):
-        if int(single_number[digit]) % 2 == 0:
-            sum_of_even_digits += int(single_number[digit])
+    for digit in range(len(current_number)):
+        if int(current_number[digit]) % 2 == 0:
+            sum_of_even_digits += int(current_number[digit])
         else:
-            sum_of_odd_digits += int(single_number[digit])
-    print(f"Odd sum = {sum_of_odd_digits},", end=" ")
-    print(f"Even sum = {sum_of_even_digits}")
+            sum_of_odd_digits += int(current_number[digit])
+    return sum_of_odd_digits, sum_of_even_digits
 
 
-make_sum_of_all_number()
+single_number = input()
+odd_digits, even_digits = make_sum_of_all_number(single_number)
+print(f"Odd sum = {odd_digits},", end=" ")
+print(f"Even sum = {even_digits}")
