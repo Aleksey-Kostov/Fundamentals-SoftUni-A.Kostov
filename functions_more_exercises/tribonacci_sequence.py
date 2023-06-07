c18f06482caa769e61bd63afd00f16282cc200a8
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-06-07 14:06:32 +0300

    SoftUni Fundamentals task

--- functions_more_exercises/tribonacci_sequence.py
+++ functions_more_exercises/tribonacci_sequence.py
@@ -1,10 +1,13 @@
 def tribonacci_sequence(num: int):
     list_current_number = [1]
     sum_current_number = 0
-    while list_current_number < list_current_number[num]:
+    while True:
         sum_current_number += 1
-        if sum_current_number == current_number + current_number:
-            list_current_number.append(current_number)
+        list_current_number.pop(0)
+        if sum_current_number == sum(list_current_number):
+            list_current_number.append(sum_current_number)
+        if len(list_current_number) == num:
+            break
     return list_current_number
 
 
