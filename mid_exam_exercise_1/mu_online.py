094b9c74cbe8309e30771b07bde61e1b27980282
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-06-16 23:21:27 +0300

    SoftUni Fundamentals task

--- mid_exam_exercise_1/mu_online.py
+++ mid_exam_exercise_1/mu_online.py
@@ -1,28 +1,39 @@
 string_input = input().split("|")
 INITIAL_HEALTH = 100
-chest_list = []
-max_string = 0
-string_index = 0
+health = INITIAL_HEALTH
+total_bitcoins = 0
+is_dead = False
+best_room = 0
 
 for string in string_input:
+    best_room += 1
     current_string = string.split()
     if current_string[0] == 'potion':
-        if INITIAL_HEALTH >= int(current_string[1]):
-            new_string = int(current_string[1])
-            print(f'You healed for {new_string} hp.')
-            print(f"Current health: {INITIAL_HEALTH + new_string} hp.")
+        current_health = int(current_string[1])
+        if INITIAL_HEALTH < current_health + health:
+            diff = INITIAL_HEALTH - health
+            health = 100
+            print(f'You healed for {diff} hp.')
+            print(f"Current health: {health} hp.")
+        else:
+            health += current_health
+            print(f'You healed for {current_health} hp.')
+            print(f"Current health: {health} hp.")
     elif current_string[0] == "chest":
-        if max_string < int(current_string[1]):
-            max_string = int(current_string[1])
-            string_index = string_input.index(string)
+        total_bitcoins += int(current_string[1])
         print(f"You found {int(current_string[1])} bitcoins.")
     else:
-        INITIAL_HEALTH -= int(current_string[1])
-        if INITIAL_HEALTH > 0:
+        health -= int(current_string[1])
+        if health > 0:
             print(f"You slayed {current_string[0]}.")
         else:
+            is_dead = True
             print(f"You died! Killed by {current_string[0]}.")
-            print(f"Best room: {string_index}")
+            print(f"Best room: {best_room}")
             break
 
 
+if not is_dead:
+    print("You've made it!")
+    print(f"Bitcoins: {total_bitcoins}")
+    print(f"Health: {health}")
