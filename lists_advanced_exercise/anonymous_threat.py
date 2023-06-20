dc31c5b1a1ca0b030c2f7bdc9bf6c1b0e41e7e1d
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-06-20 12:45:42 +0300

    SoftUni Fundamentals task

--- lists_advanced_exercise/anonymous_threat.py
+++ lists_advanced_exercise/anonymous_threat.py
@@ -4,15 +4,51 @@ command = input()
 
 def command_merge(current_list, command_1, command_2):
     new_characters = ""
-    for characters in current_list[int(command_1)]:
-        new_characters += characters
-        for char in range(int(command_2)):
-            current_list.append(new_characters)
+    if int(command_1) in range(len(current_list)) and int(command_2) in range(len(current_list)):
+        for characters in current_list[int(command_1):int(command_2) + 1]:
+            new_characters += characters
+            current_list.remove(characters)
+    elif int(command_1) in range(len(current_list)):
+        for characters in current_list[int(command_1):]:
+            new_characters += characters
+            current_list.remove(characters)
+    elif int(command_2) in range(len(current_list)):
+        for characters in current_list[int(command_2):]:
+            new_characters += characters
+            current_list.remove(characters)
+    current_list.insert(int(command_1), new_characters)
     return current_list
 
 
-def command_divide(current_list):
-    pass
+def command_divide(current_list, command_1, command_2):
+    from math import ceil
+    current_characters = ""
+    current_counter = 0
+    total_counter = 0
+    for characters in current_list[int(command_1):int(command_2) + 1]:
+        for index, digits in enumerate(characters):
+            if len(characters) % int(command_2) == 0:
+                counter = len(characters) / int(command_2)
+                if current_counter != counter:
+                    current_characters += digits
+                    current_counter += 1
+                if current_counter == counter:
+                    current_characters += " "
+                    current_counter = 0
+            else:
+                counter = len(characters) // int(command_2)
+                if current_counter != counter or counter == 0:
+                    current_characters += digits
+                    current_counter += 1
+                elif current_counter == counter:
+                    if total_counter != ceil(counter):
+                        current_characters += " "
+                        current_counter = 0
+                        total_counter += 1
+                    elif total_counter == ceil(counter):
+                        current_characters += digits
+        current_list.remove(characters)
+        current_list.append(current_characters)
     return current_list
 
 
@@ -21,7 +57,8 @@ while command != "3:1":
     if current_command[0] == "merge":
         command_merge(data_list, current_command[1], current_command[2])
     elif current_command[0] == "divide":
-        pass
+        command_divide(data_list, current_command[1], current_command[2])
+    command = input()
 
 
 print(" ".join(data_list))
