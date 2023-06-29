079732ec0c7db392d86eb119cebe095b1205e176
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-06-29 23:29:55 +0300

    SoftUni Fundamentals task

--- lab_dictionaries/students.py
+++ lab_dictionaries/students.py
@@ -13,10 +13,9 @@ while True:
     for i in range(len(new_students)):
         key = new_students[i]
         value = new_students[i + 1]
-        key_2 = new_students[i + 2]
         student_dict[key] = value
-        new[key_2] = value
-        student_dict += new
-        break
+    for j in range(len(new_students)):
+        key_3 = new_students[j + 2]
+        value = student_dict
 
 print(student_dict)
