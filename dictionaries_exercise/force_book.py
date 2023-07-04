0c6997bb8c8d7d76180b0a617ee71635eae13020
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-07-04 16:57:05 +0300

    SoftUni Fundamentals task

--- dictionaries_exercise/force_book.py
+++ dictionaries_exercise/force_book.py
@@ -5,19 +5,17 @@ while user_input != "Lumpawaroo":
     if "->" in user_input:
         change_user = user_input.split(" -> ")
         name_user, side_user = change_user[0], change_user[1]
-        if side_user not in users_dict.keys():
-            users_dict[side_user] = {name_user: 0}
-        if name_user in users_dict[side_user].keys():
-            users_dict[side_user][name_user] += 1
-        if name_user not in users_dict[side_user].keys():
-            del users_dict[name_user]
+        if name_user in users_dict.values():
+            users_dict[side_user] = [user for user in name_user if user != name_user]
+        if side_user in users_dict.keys():
+            if name_user not in users_dict[side_user]:
+                users_dict[side_user].append(name_user)
     elif "|" in user_input:
         current_user = user_input.split(" | ")
         side, name = current_user[0], current_user[1]
         if side not in users_dict.keys():
-            users_dict[side] = {name: 0}
-        users_dict[side][name] += 1
+            users_dict[side] = []
+        users_dict[side].append(name)
     user_input = input()
 
-print(change_user)
-print(current_user)
+print(users_dict)
