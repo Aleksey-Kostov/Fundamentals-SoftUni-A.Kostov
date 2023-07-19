ce8703608fcc9abe13f2502e0ef29f3e351197ec
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-07-19 13:44:42 +0300

    SoftUni Fundamentals task

--- final_exam_exercise_2/activation_key_problem.py
+++ final_exam_exercise_2/activation_key_problem.py
@@ -1,5 +1,7 @@
 activation_key = input()
 
+new_digit_low = ""
+new_digit_up = ""
 command = input().split(">>>")
 while command[0] != "Generate":
     if command[0] == "Contains":
@@ -12,13 +14,22 @@ while command[0] != "Generate":
         end_index = int(command[3])
         if command[1] == "Lower":
             for digit_low in activation_key[start_index:end_index]:
-                new_digit = digit_low.casefold()
+                new_digit_low += digit_low.lower()
+            for_replace = activation_key[start_index:end_index]
+            activation_key = activation_key.replace(for_replace, new_digit_low)
             print(activation_key)
         elif command[1] == "Upper":
             for digit_up in activation_key[start_index:end_index]:
-                activation_key.replace(digit_up, digit_up.upper())
+                new_digit_up += digit_up.upper()
+            for_replace_up = activation_key[start_index:end_index]
+            activation_key = activation_key.replace(for_replace_up, new_digit_up)
             print(activation_key)
     elif command[0] == "Slice":
-        pass
-
+        slice_start_index = int(command[1])
+        slice_end_index = int(command[2])
+        slice_for_replace = activation_key[slice_start_index:slice_end_index]
+        activation_key = activation_key.replace(slice_for_replace, "")
+        print(activation_key)
     command = input().split(">>>")
+
+print(f"Your activation key is: {activation_key}")
