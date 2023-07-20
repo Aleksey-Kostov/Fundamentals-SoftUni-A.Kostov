1eb3ef08ba4750d6fdbcad36fa5dda469effd2fb
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-07-20 09:36:26 +0300

    SoftUni Fundamentals task

--- final_exam_exercise_2/emoji_detector.py
+++ final_exam_exercise_2/emoji_detector.py
@@ -9,7 +9,7 @@ sum_ascii = 0
 
 text = input()
 pattern = r"([*]{2}|[:]{2})([A-Z][a-z]{2,})\1"
-matches = re.findall(pattern, text)
+matches = re.finditer(pattern, text)
 
 pattern_2 = r"\d+"
 matches_digits = re.findall(pattern_2, text)
@@ -21,13 +21,16 @@ for digits in current_digit:
     multiple_digits *= int(digits)
 
 for match in matches:
-    emoji_list.append(match[1])
+    emoji_list.append(match.group())
 
 for emoji in emoji_list:
-    for digits in range(len(emoji)):
-        sum_ascii += ord(emoji[digits])
+    for digits in emoji[2:-2]:
+        sum_ascii += ord(digits)
     if sum_ascii > multiple_digits:
         full_emoji_list.append(emoji)
+    sum_ascii = 0
 
-print(emoji_list)
-print(multiple_digits)
+print(f"Cool threshold: {multiple_digits}")
+print(f"{len(emoji_list)} emojis found in the text. The cool ones are:")
+for emoji in full_emoji_list:
+    print(emoji)
