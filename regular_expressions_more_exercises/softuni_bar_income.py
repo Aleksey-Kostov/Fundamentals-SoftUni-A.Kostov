a508a2bf345a3647a51fc524767c69ede85d6694
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-07-26 14:26:37 +0300

    SoftUni Fundamentals task

--- regular_expressions_more_exercises/softuni_bar_income.py
+++ regular_expressions_more_exercises/softuni_bar_income.py
@@ -9,21 +9,35 @@ while order != "end of shift":
     all_order += order
     order = input()
 
-pattern = r"%([A-Z][a-z]+)\%[^\|\$\%\.]*?\<(\w+)\>[^\|\$\%\.]*?\|([0-9]+)\|[\w\-]*?([0-9.]+[0-9])(?=\$)"
+pattern = r"\%([A-Z][a-z]+)\%[^\|\$\%\.]*?\<(\w+)\>[^\|\$\%\.]*?\|([0-9]+)\|[\w\-]*?([0-9.]+[0-9])(?=\$)"
 matches = re.finditer(pattern, all_order)
 
 for match in matches:
     product = match.group(2)
     count = int(match.group(3))
     price_product = float(match.group(4))
-    if count > 0:
-        valid_order_dict[match.group(1)] = {product: count * price_product}
+    t_price = count * price_product
+    if match.group(1) not in valid_order_dict:
+        valid_order_dict[match.group(1)] = [product]
+        valid_order_dict[match.group(1)].append(t_price)
+    else:
+        valid_order_dict[match.group(1)].append(product)
+        valid_order_dict[match.group(1)].append(t_price)
 
 for k, v in valid_order_dict.items():
     customer_name = k
-    for key, value in v.items():
-        product = key
-        price = value
-        total_price += price
-        print(f"{customer_name}: {product} - {price:.2f}")
+    if len(v) > 2:
+        for index in range(len(v)):
+            if index % 2 == 0:
+                product = v[index]
+                price = v[index + 1]
+                total_price += float(price)
+            else:
+                continue
+            print(f"{customer_name}: {product} - {float(price):.2f}")
+    else:
+        product = v[0]
+        price = v[1]
+        total_price += float(price)
+        print(f"{customer_name}: {product} - {float(price):.2f}")
 print(f"Total income: {total_price:.2f}")
