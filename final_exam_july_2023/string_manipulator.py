e74e4dfcde7f470d724acd97b60520c56cbfcc58
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-07-30 10:02:32 +0300

    SoftUni Fundamentals task

---
+++ final_exam_july_2023/string_manipulator.py
@@ -0,0 +1,43 @@
+string = input()
+
+command = input().split()
+
+while command[0] != "End":
+    action = command[0]
+    if action == "Translate":
+        character = command[1]
+        replacement = command[2]
+        string = string.replace(character, replacement)
+        print(string)
+    elif action == "Includes":
+        substring = command[1]
+        if substring in string:
+            print("True")
+        else:
+            print("False")
+    elif action == "Start":
+        subs = command[1]
+        if string.startswith(subs):
+            print("True")
+        else:
+            print("False")
+    elif action == "Lowercase":
+        string = string.lower()
+        print(string)
+    elif action == "FindIndex":
+        char = command[1]
+        index_char = 0
+        for i in range(len(string)):
+            if string[i] == char:
+                current_index_char = i
+                if current_index_char > index_char:
+                    index_char = current_index_char
+        print(index_char)
+    elif action == "Remove":
+        start_index = int(command[1])
+        count = int(command[2])
+        end_index = start_index + count
+        string = string[:start_index] + string[end_index:]
+        print(string)
+    command = input().split()
+
