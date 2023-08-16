4048d3c4a912fef4022e62ef9186c9255f06080f
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-08-16 16:44:46 +0300

    SoftUni Fundamentals task

--- more_exercises_basic_syntax_conditional_statements_and_loops/wolf_in_sheep's_clothing.py
+++ more_exercises_basic_syntax_conditional_statements_and_loops/wolf_in_sheep's_clothing.py
@@ -1,5 +1,7 @@
 string = input().split(", ")
 list_of_sheep = []
+index_wolf = 0
+number_of_sheep = 0
 
 for animal in string:
     if animal == "sheep":
@@ -7,5 +9,8 @@ for animal in string:
         number_of_sheep = len(list_of_sheep)
     elif animal == "wolf":
         index_wolf = string.index(animal)
-        if index_wolf == 0:
-            print(f"Oi! Sheep number {number_of_sheep}! You are about to be eaten by a wolf!")
+
+if index_wolf == 0:
+    print(f"Oi! Sheep number {number_of_sheep}! You are about to be eaten by a wolf!")
+else:
+    print("Please go away and stop eating my sheep")
